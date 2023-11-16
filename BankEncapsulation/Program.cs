de32972ebf0269bb34f22498632cf15a2cba7832
Morgan Kaufman <Morgankaufman13@yahoo.com>
2023-11-16 13:04:13 -0500

    finished Bank encapsulation

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you our account balance is now {account.GetBalance()}");
         }
     }
 }
